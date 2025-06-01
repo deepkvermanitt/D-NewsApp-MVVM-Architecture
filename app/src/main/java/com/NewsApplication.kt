ac0b0b178cb8565daca91b68d13cfc3b80f35d71
TFDeep <deep.nitt@gmail.com>
2025-06-01 15:46:06 +0530

    Add code to initialise Application Component

--- app/src/main/java/com/NewsApplication.kt
+++ app/src/main/java/com/NewsApplication.kt
@@ -1,10 +1,22 @@
 package com
 
 import android.app.Application
+import com.deepkverma.d_newsapp_mvvm_architecture.di.component.ApplicationComponent
+import com.deepkverma.d_newsapp_mvvm_architecture.di.component.DaggerApplicationComponent
+import com.deepkverma.d_newsapp_mvvm_architecture.di.module.ApplicationModule
 
-class NewsApplication : Application(){
+class NewsApplication : Application() {
+
+    private lateinit var applicationComponent: ApplicationComponent
     override fun onCreate() {
         super.onCreate()
+        getDependencies()
     }
 
+    private fun getDependencies() {
+        applicationComponent = DaggerApplicationComponent.builder().applicationModule(
+            ApplicationModule(this)
+        ).build()
+        //applicationComponent.inject(this)
+    }
 }
--- app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/di/component/ApplicationComponent.kt
@@ -11,4 +11,5 @@ interface ApplicationComponent {
 
     fun inject(application: NewsApplication)
 
+
 }
