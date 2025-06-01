440e5bacedea0c4b4f5acf2789215e84393532bb
TFDeep <deep.nitt@gmail.com>
2025-06-01 15:01:06 +0530

    Add repository classes for TopHeadlineRepository

--- app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/data/model/TopHeadlinesResponse.kt
+++ app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/data/model/TopHeadlinesResponse.kt
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName
 
 data class TopHeadlinesResponse(
     @SerializedName("status")
-    private val status: String = AppConstant.EMPTY,
+    val status: String = AppConstant.EMPTY,
     @SerializedName("totalResults")
-    private val totalResults: Int = 0,
-    @SerializedName("articles") private val articles: List<Article> = ArrayList<Article>()
+    val totalResults: Int = 0,
+    @SerializedName("articles") val articles: List<Article> = ArrayList<Article>()
 )
---
+++ app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/data/repository/TopHeadlineRepository.kt
@@ -0,0 +1,20 @@
+package com.deepkverma.d_newsapp_mvvm_architecture.data.repository
+
+import com.deepkverma.d_newsapp_mvvm_architecture.data.api.NetworkService
+import com.deepkverma.d_newsapp_mvvm_architecture.data.model.Article
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TopHeadlineRepository @Inject constructor(private val networkService: NetworkService) {
+    fun getTopHeadlines(country : String): Flow<List<Article>> {
+       return  flow{
+           emit(networkService.getTopHeadlines(country))
+       }.map {  it.articles }
+
+    }
+}
