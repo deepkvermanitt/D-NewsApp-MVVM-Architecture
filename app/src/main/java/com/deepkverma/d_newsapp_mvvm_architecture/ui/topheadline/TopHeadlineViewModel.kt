b9341bfe1a3f09a407196c2696ab9f8750f954af
TFDeep <deep.nitt@gmail.com>
2025-06-01 15:40:37 +0530

    Add fetchTopHeadlines method in TopHeadlineViewModel

---
+++ app/src/main/java/com/deepkverma/d_newsapp_mvvm_architecture/ui/topheadline/TopHeadlineViewModel.kt
@@ -0,0 +1,34 @@
+package com.deepkverma.d_newsapp_mvvm_architecture.ui.topheadline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deepkverma.d_newsapp_mvvm_architecture.data.model.Article
+import com.deepkverma.d_newsapp_mvvm_architecture.data.repository.TopHeadlineRepository
+import com.deepkverma.d_newsapp_mvvm_architecture.ui.UiState
+import com.deepkverma.d_newsapp_mvvm_architecture.utils.AppConstant.COUNTRY
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class TopHeadlineViewModel(private val topHeadlineRepository: TopHeadlineRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<UiState<List<Article>>>(UiState.Loading)
+    val uiState: StateFlow<UiState<List<Article>>> = _uiState
+
+    init {
+        fetchTopHeadlines()
+    }
+
+    private fun fetchTopHeadlines() {
+        viewModelScope.launch {
+            topHeadlineRepository.getTopHeadlines(COUNTRY).catch {
+
+                _uiState.value = UiState.Error(it.message.toString())
+            }
+                .collect {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+
+    }
+}
